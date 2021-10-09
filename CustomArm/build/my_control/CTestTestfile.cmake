522df603890bed231e46849beba542ee75ba1a01
Yash Jangir <offjangir@gmail.com>
2021-10-10 01:05:37 +0530

    inital stack

--- CustomArm/build/my_control/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/reaperj/arm_git/Robotic_arm-main/CustomArm/src/my_control
-# Build directory: /home/reaperj/arm_git/Robotic_arm-main/CustomArm/build/my_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
